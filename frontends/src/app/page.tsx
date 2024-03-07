467a12d1e0e3ffa40725eda60d142b9e667e8d7f
UD2105 <yudai.manage969@gmail.com>
2024-03-07 13:08:14 +0900

    chakra　ダイアログを表示

--- frontends/src/app/page.tsx
+++ frontends/src/app/page.tsx
@@ -1,19 +1,70 @@
 "use client"
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import { ChakraProvider } from '@chakra-ui/react'
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure
+} from '@chakra-ui/react'
 
-export default function Calendar() {
+export default function App() {
+  // 2. Wrap ChakraProvider at the root of your app
   return (
-    <FullCalendar
-      plugins={[ dayGridPlugin ]}
-      initialView="dayGridWeek"
-      weekends={false}
-      events={[
-        { title: 'event 1', date: '2024-03-015' },
-        { title: 'event 2', date: '2024-03-16' }
-      ]}
-    />
+    <ChakraProvider>
+      <Calendar />
+    </ChakraProvider>
+  )
+}
 
+function ManualClose() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  return (
+    <>
+      <Button onClick={onOpen}>Open Modal</Button>
+
+      <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create your account</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3}>
+              Save
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
+
+function Calendar() {
+  return (
+    <div>
+      <ManualClose/>
+      <FullCalendar
+        plugins={[ dayGridPlugin ]}
+        initialView="dayGridWeek"
+        weekends={false}
+        events={[
+          { title: 'event 1', date: '2024-03-015' },
+          { title: 'event 2', date: '2024-03-16' }
+        ]}
+      />
+    </div>
   )
 }
 
