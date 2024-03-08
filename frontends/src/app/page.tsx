25a53565d2995e540e446ec8de5c58cf9038dfb7
UD2105 <yudai.manage969@gmail.com>
2024-03-08 11:14:39 +0900

    callender event viewed

--- frontends/src/app/page.tsx
+++ frontends/src/app/page.tsx
@@ -4,6 +4,7 @@ import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 import { ChakraProvider } from '@chakra-ui/react'
 import { useEffect, useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form"
+import { sliceEvents, createPlugin } from '@fullcalendar/core';
 
 type Inputs = {
   title: string
@@ -117,7 +118,15 @@ function AddEventDialog() {
 
 function Calendar() {
   const [events, setEvents] = useState([]);
-
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { register,handleSubmit,watch,formState: { errors }} = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = (data) => [fetch('http://127.0.0.1:8000/users/1/events/', {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data)
+  })]
   const handleFetch = async () => {
     const response = await fetch('http://127.0.0.1:8000/events/user/1/', {
       headers: {
@@ -134,17 +143,73 @@ function Calendar() {
     handleFetch();
   }, []);
 
+
+  const handleOpen =()=>{
+    onOpen()
+  }
+
   return (
     <div>
       <AddEventDialog/>
+      <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <form onSubmit={handleSubmit(onSubmit)}>
+        <ModalContent>
+          <ModalHeader>
+            <Center>
+              <Box w ="80%">
+                <Input placeholder='イベントタイトル' {...register("title", { required: true })}  />
+              </Box>
+            </Center>
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+          <VStack>
+            <Input
+                placeholder="Select Date and Time"
+                size="md"
+                type="datetime-local"
+                {...register("date", { required: true })}
+              />
+              <Textarea placeholder='Here is a sample placeholder' {...register("description", { required: true })}/>
+              {errors.description && <span>This field is required</span>}
+
+          </VStack>
+
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} type = "submit">
+              Add events
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+        </form>
+      </Modal>
       <FullCalendar
-        plugins={[ dayGridPlugin ]}
+        plugins={[ dayGridPlugin]}
+        eventContent={generateRenderEventContent(handleOpen)}
         initialView="dayGridWeek"
         weekends={false}
         events={events}
+        
       />
     </div>
   )
+  
 }
 
+function generateRenderEventContent(handleOpen){
+  function renderEventContent(eventInfo) {
+    return(
+      <>
+        <b>{eventInfo.timeText}</b>
+        <i>{eventInfo.event.title}</i>
+        <Button onClick={handleOpen}>test</Button>
+      </>
+    )
+  }
 
+  return renderEventContent
+}
